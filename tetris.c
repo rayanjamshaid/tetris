f57f6914871b5c93a68fa7309c465d25f2f2a4d2
Code;Gate <rayanjamshaid09@gmail.com>
2025-07-18 18:52:05 +0500

    added macros

--- tetris.c
+++ tetris.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define ROWS 18
+#define COLS 10
+
+#define BLOCK_ROW 2
+#define BLOCK_COL 4
+
 void initiate_grid(char ** grid)
 {
-    for(int i=0;i<18;i++)
+
+    for(int i=0;i<ROWS;i++)
     {
-        for(int j=0;j<10;j++)
+        for(int j=0;j<COLS;j++)
         {
             grid[i][j]='.';
         }
@@ -15,10 +22,10 @@ void initiate_grid(char ** grid)
 void print_grid(char ** grid)
 {
     printf("!|/\\/\\/\\/\\/\\|!\n");
-    for(int i=0;i<18;i++)
+    for(int i=0;i<ROWS;i++)
     {
         printf("!|");
-        for(int j=0;j<10;j++)
+        for(int j=0;j<COLS;j++)
         {
             printf("%c",grid[i][j]);
         }
@@ -41,19 +48,39 @@ void clr_scr()
 }
 
 
+void gameplay()
+{
+    
+}
+
 int main()
 {
-    char **grid=(char**)malloc(18 * sizeof(char*));
-    for(int i=0;i<18;i++)
+    /*starting making the grids and blocks*/
+    char **grid=(char**)malloc(ROWS * sizeof(char*));
+    for(int i=0;i<ROWS;i++)
     {
-        grid[i]=(char*)malloc(11 * sizeof(char));
+        grid[i]=(char*)malloc((COLS+1) * sizeof(char));
     }
 
     initiate_grid(grid);
 
+
+    char **bar=(char**)malloc(BLOCK_ROW * sizeof(char*));
+    for(int i=0;i<2;i++)
+    {
+        bar[i]=(char*)malloc((BLOCK_COL+1) * sizeof(char));
+    }
+    bar[0][0]='#'; bar[0][1]='#'; bar[0][2]='#'; bar[0][3]='#'; bar[0][4]='\0';
+    bar[1][0]='.'; bar[1][1]='.'; bar[1][2]='.'; bar[1][3]='.'; bar[1][4]='\0';
+
+
+    /*ending making the grids and blocks */
+
+
     while(1)
     {
         print_grid(grid);
+        gameplay();
         clr_scr();
     }
     
